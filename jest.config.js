b426628bd3e1b29f58f457bfeb2bed548fcc6e8c
jucieltonsantos <jucielton_santos@hotmail.com>
2025-07-25 17:04:41 -0300

    chore: ajustar ci

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,25 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+      },
+    ],
   },
-  testMatch: ["**/?(*.)+(test).[tj]s?(x)"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
