872776c39550c51b54dfea5df30296f8b1b16339
jucieltonsantos <jucielton_santos@hotmail.com>
2025-07-25 17:26:26 -0300

    ajustar mensagem login

--- src/app/login/login.messages.ts
+++ src/app/login/login.messages.ts
@@ -1,6 +1,7 @@
 export const STATUS_MESSAGES = {
-  success: "Login efetuado com sucesso!",
+  success: 'Login efetuado com sucesso!',
   form: {
-    minCarcter: "A senha deve ter no mínimo 8 caracteres",
+    minCarcterPassWord: 'A senha deve ter no mínimo 8 caracteres',
+    minCarcterUser: 'O usuário deve ter no mínimo 3 caracteres',
   },
 };
